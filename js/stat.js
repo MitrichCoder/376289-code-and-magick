0143757334b0107c6fdce2822a22a6a912016bca
Dmitriy Sheshkov <mts3656060@yandex.ru>
2018-06-05 12:45:01 +0300

    Исправляет ошибки (npm test)

--- js/stat.js
+++ js/stat.js
@@ -9,12 +9,12 @@ var BAR_WIDTH = 40; // ширина столба гистограммы
 var BAR_INTERVAL = 50; // расстояние межту столбцами гистограммы
 var BARCHART_HEIGHT = 150; // высота гистограммы
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -26,7 +26,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
