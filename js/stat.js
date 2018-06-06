c93e23d92dd83d0ecd6dbeb283a589bb3cba5aa5
Dmitriy Sheshkov <mts3656060@yandex.ru>
2018-06-06 09:23:17 +0300

    Дорабатывает ДЗ

--- js/stat.js
+++ js/stat.js
@@ -26,6 +26,30 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getBarColor = function (ctx, player) {
+  if (player === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = 'hsla(235, ' + Math.random() * 100 + '%, 29%, 1)';
+  }
+}
+
+var drawBar = function (ctx, players, time, order, times) {
+  var maxTime = getMaxElement(times);
+
+  var BAR_HEIGHT = Math.floor(BARCHART_HEIGHT * time / maxTime); // расчёт высоты столбца гистограммы текущего игрока
+  var BAR_XPOINT = CLOUD_X + GAP * 4 + ((BAR_WIDTH + BAR_INTERVAL) * order); // положение текущего столбца и подписи гистограммы по оси X
+  var BAR_YPOINT = CLOUD_Y + CLOUD_HEIGHT - (GAP * 3) - BAR_HEIGHT; // положение верхнего левого угла гистограммы по оси Y
+  var PLAYER_TIME = Math.floor(time); // показатель времени прохождения игры текущим игроком
+
+  ctx.fillStyle = '#000000';
+  ctx.fillText(players, BAR_XPOINT, CLOUD_Y + CLOUD_HEIGHT - GAP * 2.5);
+  ctx.fillText(PLAYER_TIME, BAR_XPOINT, CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - GAP * 5);
+
+  getBarColor(ctx, players);
+  ctx.fillRect(BAR_XPOINT, BAR_YPOINT, BAR_WIDTH, BAR_HEIGHT);
+}
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
@@ -37,28 +61,7 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 2);
   ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP * 4);
 
-  var maxTime = getMaxElement(times);
-  // console.log('Максимальное значение в гистограмме: ' + maxTime);
-
   for (var i = 0; i < players.length; i++) {
-
-    var BAR_HEIGHT = Math.floor(BARCHART_HEIGHT * times[i] / maxTime); // расчёт высоты столбца гистограммы текущего игрока
-    var BAR_XPOINT = CLOUD_X + GAP * 4 + ((BAR_WIDTH + BAR_INTERVAL) * i); // положение текущего столбца и подписи гистограммы по оси X
-    var BAR_YPOINT = CLOUD_Y + CLOUD_HEIGHT - (GAP * 3) - BAR_HEIGHT; // положение верхнего левого угла гистограммы по оси Y
-    var PLAYER_TIME = Math.floor(times[i]); // показатель времени прохождения игры текущим игроком
-
-    ctx.fillStyle = '#000000';
-    ctx.fillText(players[i], BAR_XPOINT, CLOUD_Y + CLOUD_HEIGHT - GAP * 2.5);
-    ctx.fillText(PLAYER_TIME, BAR_XPOINT, CLOUD_Y + CLOUD_HEIGHT - BAR_HEIGHT - GAP * 5);
-
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'hsla(235, ' + Math.random() * 100 + '%, 29%, 1)';
-    }
-
-    ctx.fillRect(BAR_XPOINT, BAR_YPOINT, BAR_WIDTH, BAR_HEIGHT);
-
-    // console.log('Игрок ' + players[i] + ' справился с задачей за ' + PLAYER_TIME / 1000 + ' секунд. Высота столбца гистограммы: ' + BAR_HEIGHT + 'px');
+    drawBar(ctx, players[i], times[i], i, times);
   }
 };
